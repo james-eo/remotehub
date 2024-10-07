fb4cc086907df6bbad557c89743f02554db603c7
james-eo <jameseokonkwo@gmail.com>
2024-10-08 00:15:16 +0100

    Created user routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  allUsers,
+  deleteUser,
+  singleUser,
+  updateUser,
+} from "../controllers/userController.js";
+import { isAuthenticated, isAdmin } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// user routes
+// /api/user/allusers
+router.get("/allusers", isAuthenticated, isAdmin, allUsers);
+// /api/user/id
+router.get("/user/:id", isAuthenticated, singleUser);
+// /api/user/update/id
+router.put("/user/update/:id", isAuthenticated, updateUser);
+// /api/user/delete/id
+router.delete("/admin/user/delete/:id", isAuthenticated, isAdmin, deleteUser);
+
+export default router;
