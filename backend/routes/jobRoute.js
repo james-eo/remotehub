2bc736daaf6e5f11916dce3c2d949db1a0e095c9
james-eo <jameseokonkwo@gmail.com>
2024-10-09 07:14:22 +0100

    Created jobRoute Component

---
+++ backend/routes/jobRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import {
+  createJob,
+  singleJob,
+  updateJob,
+  showJobs,
+} from "../controllers/jobController.js";
+import { isAuthenticated, isAdmin } from "../middleware/authMiddleware.js";
+
+// Jobs routes
+
+// /api/job/create
+router.post("/job/create", isAuthenticated, isAdmin, createJob);
+
+// /api/job/:id
+router.get("/job/:id", singleJob);
+
+// /api/job/update/:job_id
+router.put("/job/update/:job_id", isAuthenticated, isAdmin, updateJob);
+
+// /api/jobs/show
+router.get("/jobs/show", showJobs);
+
+export default router;
