847f17201e00a9109249eb4e42a88725ddc0f026
james-eo <jameseokonkwo@gmail.com>
2024-10-15 14:16:30 +0100

    Created error boundry component

---
+++ frontend/src/components/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+  };
+
+  // Update state when an error occurs to display fallback UI
+  public static getDerivedStateFromError(_: Error): State {
+    return { hasError: true };
+  }
+
+  // Log the error information for debugging
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render(): ReactNode {
+    if (this.state.hasError) {
+      // Render fallback UI if an error occurred
+      return (
+        <div>
+          <h1>Something went wrong.</h1>
+          <p>Please try again later.</p>
+        </div>
+      );
+    }
+
+    // Render children components if no error
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
